b352514ede0952fba67adfbf024610fed7ce3eed
fredalbert37 <fredalbert37@outlook.com>
2022-09-04 04:49:03 -0500

    home page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,44 @@
 <template>
-  <router-view/>
+  <div>
+    <div v-if="!mobile" class="app flex flex-column">
+      <Navigation />
+      <div class="app-content flex flex-column">
+        <router-view />
+      </div>
+    </div>
+    <div v-else class="mobile-message flex flex-column">
+      <h2>Sorry, this app is not supported on Mobile Devices</h2>
+      <p>To use this app, please use a computer or tablet</p>
+    </div>
+  </div>
 </template>
+<script>
+import Navigation from './components/Navigation.vue';
+  export default{
+    components: {
+      Navigation,
+    },
+    data() {
+      return {
+        mobile: null
+      }
+    },
+    created() {
+      this.checkScreen();
+      window.addEventListener('resize', this.checkScreen);
+    },
+    methods: {
+      checkScreen() {
+        const windowWidth = window.innerWidth;
+        if(windowWidth <= 750){
+          this.mobile = true;
+          return;
+        }
+        this.mobile = false;
+      }
+    },
+  }
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
@@ -10,7 +48,32 @@
   padding: 0;
   box-sizing: border-box;
   font-family: "Poppins", sans-serif;
+}
+
+.app {
   background-color: #141625;
+  min-height: 100vh;
+  @media(min-width: 900px){
+    flex-direction: row !important;
+  }
+
+  &-content{
+   padding: 0 20px;
+   flex:1;
+   position: relative; 
+  }
+}
+
+.mobile-message {
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #141625;
+  color: #f3f3f3;
+  p {
+    margin-top: 1.6rem;
+  }
 }
 
 button,
